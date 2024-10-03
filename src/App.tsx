de54208a89ee2f155677e2da67610a7ce0cfe30e
Naufil Asar <naufilasar.dev@gmail.com>
2024-10-03 19:40:48 +0530

    Initialized base project with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='h-screen bg-gray-800'>
+    <p className='text-red-500'>Hellow</p>
+    </div>
+  )
+}
+
+export default App
